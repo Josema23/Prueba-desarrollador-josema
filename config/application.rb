e743ace3b998f7564756acd975545b6d73aca40b
Josema Luque <josema.jmlo@gmail.com>
2019-11-23 13:35:32 +0100

    Configuración de MongoDB usando mongoid

--- config/application.rb
+++ config/application.rb
@@ -2,20 +2,33 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+# require "active_model/railtie"
+# require "active_job/railtie"
 # require "active_record/railtie"
 # require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+%w(
+  mongoid
+  action_controller
+  action_mailer
+  active_resource
+  rails/test_unit
+).each do |framework|
+begin
+  require "#{framework}/railtie"
+    rescue LoadError
+  end
+end
 
 module PruebaDesarrolladorJosema
   class Application < Rails::Application
